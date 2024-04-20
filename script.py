dbb68c554fce5e20fc18c886855ea96adabdedbe
John Doe <j.donatmagnin@it-students.fr>
2024-04-20 17:03:13 +0200

    Ex 22 ne fonctionne pas

--- script.py
+++ script.py
@@ -169,6 +169,119 @@ def exo13():
    test_tuple = (10, 20, 30, 40, 50)
    print("Max, Min, Moyenne:", max_min_moy(test_tuple))
 
+def sort_students(etuds):
+   sort_studs = []
+   for x in range(len(etuds)):
+      max_ind = -1
+      max_val = -1
+      for y in range(len(etuds)):
+         if(etuds[y][1]>max_val):
+            max_ind = y
+            max_val = etuds[y][1]
+      sort_studs.append((etuds[max_ind][0], etuds[max_ind][1]))
+      etuds.pop(max_ind)
+   return sort_studs
+
+def exo14():
+   etudiants = [("Alice", 88), ("Bob", 95), ("Charlie", 78)]
+   etudiants_tries = sort_students(etudiants)
+   print(etudiants_tries)
+
+def tri_actif(d):
+   ret = []
+   for x in d:
+      if(x[2]=="actif"):
+         ret.append(x)
+   return ret
+
+def tri_nb_desc(d):
+   sort_tuple = []
+   for x in range(len(d)):
+      max_ind = -1
+      max_val = -1
+      for y in range(len(d)):
+         if(d[y][1]>max_val):
+            max_ind = y
+            max_val = d[y][1]
+      sort_tuple.append((d[max_ind][0], d[max_ind][1], d[max_ind][2]))
+      d.pop(max_ind)
+   return sort_tuple
+
+def filtre_stat(d):
+   tup = []
+   for e in d:
+      tup.append(e[0], e[1])
+   return tup
+
+
+def exo15():
+   data = [("id1", 10, "actif"), ("id2", 15, "inactif"), ("id3", 20, "actif"), ("id4", 15, "inactif"), ("id5", 32, "actif"), ("id6", 26, "inactif")]
+   ssmenu = input("Entrez A pour trier les actifs\nD pour trier par nombre décroissant\nS pour filtrer sans statut\n")
+   match ssmenu:
+      case 'A':
+         print(tri_actif(data))
+      case 'D':
+         print(tri_nb_desc(data))
+      case _:
+         print(filtre_stat(data))
+
+def exo16():
+   livre = { "titre": "Les Misérables", "auteur": "Victor Hugo", "annee": "1862"}
+   return livre["auteur"]
+
+def exo17():
+   livre = { "titre": "Les Misérables", "auteur": "Victor Hugo", "annee": "1862"}
+   livre["annee"]="1865"
+   livre["genre"]="Roman"
+   return livre
+
+def affiche_dict(d):
+   for e in d.items():
+      print(e)
+
+def exo18():
+   livre = exo17()
+   affiche_dict(livre)
+
+def exo19():
+   dic1 = {"un": 1, "deux":2, "trois": 3}
+   dic2 = {"quatre": 4, "cinq": 5, "six": 6}
+   dico = {**dic1, **dic2}
+   print(dico)
+
+def exo20(d1, d2):
+   d = []
+   for x1, x2  in d2.items():
+      for y1, y2 in d1.items():
+         if (x2 == y1):
+            z = {x1: y2}
+            d.append(z)
+   print(d)
+
+def intersection_without_multiples_of_three(s1,s2):
+   s = s1 & s2
+   sr = {x for x in s if x%3!=0}
+   #for x in s:
+   #   if x % 3 != 0:
+   #      sr 
+   #print(sr)
+   return sr
+
+def exo21():
+   set1 = {1, 2, 3, 4, 6, 9, 10}
+   set2 = {2, 3, 5, 6, 7, 9, 10}
+   print(intersection_without_multiples_of_three(set1, set2))
+
+def union_excluding_n(lis, n):
+   s = []
+   for y in lis:
+      s.append({x for x in y if(x != n)})
+   return s
+
+def exo22():
+   list_of_sets = [{1, 2, 3}, {3, 4, 5}, {5, 6, 7}]
+   n = 5
+   print(union_excluding_n(list_of_sets, n))
 
 menu = int(input("Entrez un chiffre pour acceder a l exercice (de 1 a 22)\n"))
 match menu:
@@ -198,6 +311,24 @@ match menu:
       exo12()
    case 13:
       exo13()
+   case 14:
+      exo14()
+   case 15:
+      exo15()
+   case 16:
+      print(exo16())
+   case 17:
+      print(exo17())
+   case 18:
+      exo18()
+   case 19:
+      exo19()
+   case 20:
+      exo20({"a": 1, "b": 2, "c": 3}, {"x": "a", "y": "b"})
+   case 21:
+      exo21()
+   case 22:
+      exo22()
    case _:
       print("Erreur : Veuillez rentrer un chiffre compris entre 1 et 2\n")
 
